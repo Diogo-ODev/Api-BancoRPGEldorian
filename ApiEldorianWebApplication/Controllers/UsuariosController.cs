e08663fee43bf4542c922f16e3beeea1a36a83f7
pablorenan-dev <pablonichterwistz@outlook.com>
2024-12-22 18:29:52 -0300

    criado o GetUsuario e o PosUsuario

---
+++ ApiEldorianWebApplication/Controllers/UsuariosController.cs
@@ -0,0 +1,32 @@
+﻿using ApiEldorianWebApplication.BancoDeDados;
+using ApiEldorianWebApplication.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEldorianWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly BancoContext _context;
+        public UsuariosController(BancoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            
+            _context.SaveChangesAsync();
+            return CreatedAtAction("GetUsuario", new { id = usuario.idUsuario }, usuario);
+        }
+    }
+}
